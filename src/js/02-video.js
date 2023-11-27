13b7f62298913ad3be4eb5bed6c57dbcfe9cb1a4
Anastasia-Kostenko <nanochka03@gmail.com>
2023-11-27 23:00:04 +0200

    corection_task_02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,26 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
 const onPlay = function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds)
+    localStorage.setItem('videoplayer-current-time', data.seconds);
 };
-
-const setCurrentTime = Number(localStorage.getItem
-    ('videoplayer-current-time'));
-
-player.setCurrentTime(currentTime).then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function (error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
-
 player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem(CURRENT_TIME_KEY));
+// console.log(currentTime);
+player
+    .setCurrentTime(currentTime)
+    .then(function (seconds) { })
+    .catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+            default:
+                break;
+        }
+    });
